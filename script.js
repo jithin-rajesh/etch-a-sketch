165187b290dae6ccbbd0b30af6c65cbf744d9c4b
jithin-rajesh <jithin.ayalloor@gmail.com>
2024-12-11 17:12:00 +0530

    Added functionalities like changing divs, reset button and a color input

--- script.js
+++ script.js
@@ -1,9 +1,89 @@
 let container = document.querySelector(".container");
 
-for (let i = 0; i< 256; i++)
+let range = 32;
+
+
+
+let slider = document.querySelector(".slider");
+
+slider.addEventListener("input", function()
 {
-    const newDiv = document.createElement('div');
-    newDiv.classList.add("newDiv");
-    newDiv.setAttribute("style", `background-color: white; height: ${30}; width : 30px; margin: 0 0; `);
-    container.appendChild(newDiv);
+    clearColors();
+    let rangeText = document.querySelector(".rangetext");
+    rangeText.textContent = `${this.value} x ${this.value}`;
+
+    range = this.value;
+    updateContainer();
+})
+
+
+let newDivs = [];
+function updateContainer()
+{
+    const currentDivs = container.children.length;
+    const totalDivs = range * range;
+    const boxSize = 480/range;
+
+    if (currentDivs > totalDivs)
+    {
+        for (let i = currentDivs - 1; i>=totalDivs; i--)
+        {
+            container.removeChild(container.lastChild);
+        }
+    }
+    else if (currentDivs < totalDivs)
+    {
+        for (let i = currentDivs; i < totalDivs; i++)
+        {
+            let newDiv = document.createElement('div');
+            newDiv.classList.add("newDiv");
+            newDiv.addEventListener('mouseover',function()
+            {
+                newDiv.style.backgroundColor = "white";
+            });
+            container.appendChild(newDiv);
+            newDivs.push(newDiv);
+
+            
+        }
+
+    }
+
+    Array.from(container.children).forEach(newDiv => {
+        newDiv.style.width = `${boxSize}px`;
+        newDiv.style.height = `${boxSize}px`;
+    });
+
 }
+function clearColors()
+    {
+        console.log('RESET');
+        newDivs.forEach(div =>
+        {
+            div.style.backgroundColor = "black";
+        }
+        );
+    }
+resetButton = document.querySelector('#reset');
+resetButton.addEventListener('click', clearColors());
+
+updateContainer();
+
+
+function clearColors()
+{
+    console.log('RESET');
+    newDivs.forEach(div =>
+    {
+        div.style.backgroundColor = "black";
+    }
+    );
+}
+resetButton = document.querySelector('#reset');
+resetButton.addEventListener('click', clearColors);
+
+
+
+
+
+
