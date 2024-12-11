5c9c0ba7dc816de5bae1b896fc51155d01ae7e57
jithin-rajesh <jithin.ayalloor@gmail.com>
2024-12-11 18:32:47 +0530

    Aded a rainbow button that randomizes colors, debugged effectively

--- script.js
+++ script.js
@@ -2,12 +2,9 @@ let container = document.querySelector(".container");
 
 let range = 32;
 
-
-
 let slider = document.querySelector(".slider");
 
-slider.addEventListener("input", function()
-{
+slider.addEventListener("input", function () {
     clearColors();
     let rangeText = document.querySelector(".rangetext");
     rangeText.textContent = `${this.value} x ${this.value}`;
@@ -16,74 +13,77 @@ slider.addEventListener("input", function()
     updateContainer();
 })
 
+let color = "#20df1a"; 
 
 let newDivs = [];
-function updateContainer()
-{
+let isRainbow = false; 
+
+function updateContainer() {
     const currentDivs = container.children.length;
     const totalDivs = range * range;
-    const boxSize = 480/range;
+    const boxSize = 480 / range;
 
-    if (currentDivs > totalDivs)
-    {
-        for (let i = currentDivs - 1; i>=totalDivs; i--)
-        {
+    if (currentDivs > totalDivs) {
+        for (let i = currentDivs - 1; i >= totalDivs; i--) {
             container.removeChild(container.lastChild);
         }
-    }
-    else if (currentDivs < totalDivs)
-    {
-        for (let i = currentDivs; i < totalDivs; i++)
-        {
+    } else if (currentDivs < totalDivs) {
+        for (let i = currentDivs; i < totalDivs; i++) {
             let newDiv = document.createElement('div');
             newDiv.classList.add("newDiv");
-            newDiv.addEventListener('mouseover',function()
-            {
-                newDiv.style.backgroundColor = "white";
+
+            newDiv.addEventListener('mouseover', function () {
+                if (isRainbow) {
+                    newDiv.style.backgroundColor = rainbow();
+                } else {
+                    newDiv.style.backgroundColor = `${color}`;
+                }
             });
+
             container.appendChild(newDiv);
             newDivs.push(newDiv);
-
-            
         }
-
     }
 
     Array.from(container.children).forEach(newDiv => {
         newDiv.style.width = `${boxSize}px`;
         newDiv.style.height = `${boxSize}px`;
     });
-
 }
-function clearColors()
-    {
-        console.log('RESET');
-        newDivs.forEach(div =>
-        {
-            div.style.backgroundColor = "black";
-        }
-        );
-    }
+
+function clearColors() {
+    console.log('RESET');
+    newDivs.forEach(div => {
+        div.style.backgroundColor = "black";
+    });
+}
+
+function setToMonochrome() {
+    color = '#20df1a'; 
+    isRainbow = false; 
+}
+
+function rainbow() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`; 
+}
+
 resetButton = document.querySelector('#reset');
-resetButton.addEventListener('click', clearColors());
+resetButton.addEventListener('click', clearColors); 
+
+monoButton = document.querySelector('#monochrome');
+monoButton.addEventListener('click', setToMonochrome);
+
+rainbowButton = document.querySelector('#rainbow');
+rainbowButton.addEventListener('click', function () {
+    isRainbow = true; 
+}); 
+
+colorPicker = document.querySelector('#color-selector');
+colorPicker.addEventListener("change", changeColor); 
+
+function changeColor(e) {
+    color = e.target.value; 
+    isRainbow = false; 
+}
 
 updateContainer();
-
-
-function clearColors()
-{
-    console.log('RESET');
-    newDivs.forEach(div =>
-    {
-        div.style.backgroundColor = "black";
-    }
-    );
-}
-resetButton = document.querySelector('#reset');
-resetButton.addEventListener('click', clearColors);
-
-
-
-
-
-
